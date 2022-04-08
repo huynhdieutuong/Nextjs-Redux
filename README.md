17b112bbdf92ea56888dfda4963438ea126f8593
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2022-04-08 17:08:01 +0700

    SWR to get data for client side

--- README.md
+++ README.md
@@ -10,3 +10,39 @@
 
 - Call api getUserById in Client side
 - SetCurrentUser as global state by UserContext
+
+# SWR
+
+It is the same **redux** or **context API** to share state for components:
+
+- Create useUser hook
+
+```
+function useUser (id) {
+  const { data, error } = useSWR(`/api/user/${id}`, fetcher)
+
+  return {
+    user: data,
+    isLoading: !error && !data,
+    isError: error
+  }
+}
+```
+
+- Then use it in many components, it will share user
+
+```
+function Content () {
+  const { user, isLoading } = useUser()
+  if (isLoading) return <Spinner />
+  return <h1>Welcome back, {user.name}</h1>
+}
+
+function Avatar () {
+  const { user, isLoading } = useUser()
+  if (isLoading) return <Spinner />
+  return <img src={user.avatar} alt={user.name} />
+}
+```
+
+The most beautiful thing is that there will be only **1 request** sent to the API, because they use **the same SWR key** and the request is **deduped**, **cached** and **shared** automatically.
--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,28 +1,35 @@
 import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import useSWR, { Fetcher } from 'swr'
 import PostListNoSidebar from '../../components/Post/PostListNoSidebar'
 import { UserDetails } from '../../components/User'
+import { PostType } from '../../interfaces/post'
+import { CurrentUserType } from '../../interfaces/user'
 import postService from '../../services/post'
 import userService from '../../services/user'
 
+interface FetcherType {
+  user: CurrentUserType
+  posts: PostType[]
+}
+
 const Profile = () => {
   const router = useRouter()
-  const [user, setUser] = useState(null)
-  const [posts, setPosts] = useState([])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const userId = router.query.userId as string
-      const userPros = userService.getUserById(parseInt(userId))
-      const postsPros = postService.getPostListByUserId(userId)
-      const [resUser, resPosts] = await Promise.all([userPros, postsPros])
-
-      setUser(resUser.data.user)
-      setPosts(resPosts.data.posts || [])
+  const userId = router.query.userId as string
+  const fetcher: Fetcher<FetcherType> = async (userId: string) => {
+    const userPros = userService.getUserById(parseInt(userId))
+    const postsPros = postService.getPostListByUserId(userId)
+    const [resUser, resPosts] = await Promise.all([userPros, postsPros])
+    return {
+      user: resUser.data.user,
+      posts: resPosts.data.posts,
     }
+  }
 
-    fetchData()
-  }, [router.query.userId])
+  const { data } = useSWR(userId, fetcher)
+  const user = data?.user || null
+  const posts = data?.posts || []
 
   return (
     <div className='container'>
